c4107b3b1e3728a4b78b26203241d614e2190e5d
grant <grantmossdev@gmail.com>
2021-05-18 12:07:13 -0600

    set up basic run/capacity charts

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,3 +1,150 @@
+import Highcharts from "highcharts";
 import data from "./data_management/runs.json";
 
-console.log(data);
+
+function syncExtremes(e) {
+  const thisChart = this.chart;
+
+  if (e.trigger !== "syncExtremes") {
+    // Prevent feedback loop
+    Highcharts.charts.forEach((chart) => {
+      if (chart !== thisChart) {
+        if (chart.xAxis[0].setExtremes) {
+          // It is null while updating
+          chart.xAxis[0].setExtremes(e.min, e.max, undefined, false, {
+            trigger: "syncExtremes",
+          });
+        }
+      }
+    });
+  }
+}
+
+const cerPalette = {
+  "Night Sky": "#054169",
+  Sun: "#FFBE4B",
+  Ocean: "#5FBEE6",
+  Forest: "#559B37",
+  Flame: "#FF821E",
+  Aubergine: "#871455",
+  "Dim Grey": "#8c8c96",
+  "Cool Grey": "#42464B",
+  hcBlue: "#7cb5ec",
+  hcGreen: "#90ed7d",
+  hcPink: "#f15c80",
+  hcRed: "#f45b5b",
+  hcAqua: "#2b908f",
+  hcPurple: "#8085e9",
+  hcLightBlue: "#91e8e1",
+};
+
+function seriesify(runData) {
+  const addToSeries = (series, row) => {
+    series.data[0].data.push([row.d, row.v]);
+    series.data[1].data.push([row.d, row.c]);
+    return series;
+  };
+  let [west, east, quebec] = [
+    {
+      div: "runs-west",
+      data: [
+        {
+          name: "Western Canada",
+          data: [],
+          color: cerPalette["Night Sky"],
+          type: "area",
+        },
+        {
+          name: "Capacity",
+          data: [],
+          color: cerPalette["Cool Grey"],
+          type: "line",
+        },
+      ],
+    },
+    {
+      div: "runs-east",
+      data: [
+        {
+          name: "Quebec & Eastern Canada",
+          data: [],
+          color: cerPalette.Flame,
+          type: "area",
+        },
+        {
+          name: "Capacity",
+          data: [],
+          color: cerPalette["Cool Grey"],
+          type: "line",
+        },
+      ],
+    },
+    {
+      div: "runs-ontario",
+      data: [
+        { name: "Ontario", data: [], color: cerPalette.Ocean, type: "area" },
+        {
+          name: "Capacity",
+          data: [],
+          color: cerPalette["Cool Grey"],
+          type: "line",
+        },
+      ],
+    },
+  ];
+  console.log(runData);
+  let maxValue = 0;
+  runData.forEach((row) => {
+    if (row.r === "w") {
+      west = addToSeries(west, row);
+    } else if (row.r === "o") {
+      east = addToSeries(east, row);
+    } else if (row.r === "q") {
+      quebec = addToSeries(quebec, row);
+    }
+    if (row.c > maxValue) {
+      maxValue = row.c;
+    }
+  });
+
+  return { west, east, quebec, maxValue };
+}
+
+function testChart(series, maxY) {
+  Highcharts.chart(series.div, {
+    chart: {
+      zoomType: "x",
+    },
+    title: {
+      text: "",
+    },
+
+    xAxis: {
+      type: "datetime",
+      crosshair: true,
+        events: {
+          setExtremes: syncExtremes,
+        },
+    },
+
+    yAxis: {
+      max: maxY,
+    },
+
+    tooltip: {
+      shared: true,
+    },
+
+    series: series.data,
+  });
+}
+
+function mainCrudeRuns() {
+  const series = seriesify(data);
+  console.log(series);
+  testChart(series.west, series.maxValue);
+  testChart(series.east, series.maxValue);
+  testChart(series.quebec, series.maxValue);
+}
+
+mainCrudeRuns();
--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,101 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-    // mode: "development",
-    mode: "production",
-    entry: "./src/dashboard.js",
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "[name].[contenthash].js",
-      publicPath: "/dist/",
-    },
-  
-    devServer: {
-      compress: true,
-    },
-  
-    devtool: false,
-  
-    plugins: [
-    //   new CopyWebpackPlugin({
-    //     patterns: [
-    //       {
-    //         from: path.resolve(__dirname, "src", "main.css"),
-    //         to: path.resolve(__dirname, "dist", "css/main.css"),
-    //       },
-    //       {
-    //         from: path.resolve(__dirname, "src", "GCWeb"),
-    //         to: path.resolve(__dirname, "dist", "GCWeb"),
-    //       },
-    //       {
-    //         from: path.resolve(__dirname, "src", "wet-boew"),
-    //         to: path.resolve(__dirname, "dist", "wet-boew"),
-    //       },
-    //     ],
-    //   }),
-      new CleanWebpackPlugin(),
-      // uncomment these lines below for easier browser debugging in development mode
-      // new webpack.SourceMapDevToolPlugin({
-      //   filename: "dist/[file].map",
-      //   fileContext: "public",
-      // }),
-      // new BundleAnalyzerPlugin(),
-    ],
-  
-    resolve: {
-      extensions: ["*", ".js"],
-    },
-  
-    module: {
-      rules: [
+  mode: "development",
+  // mode: "production",
+  entry: "./src/dashboard.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+  },
+
+  devServer: {
+    compress: true,
+    contentBase: "./dist",
+    publicPath: "/",
+    hot: true
+  },
+
+  devtool: false,
+
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
         {
-          test: /\.(js)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
+          from: path.resolve(__dirname, "src", "main.css"),
+          to: path.resolve(__dirname, "dist", "css/main.css"),
         },
         {
-          test: /\.css$/,
-          use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+          from: path.resolve(__dirname, "src", "GCWeb"),
+          to: path.resolve(__dirname, "dist", "GCWeb"),
+        },
+        {
+          from: path.resolve(__dirname, "src", "wet-boew"),
+          to: path.resolve(__dirname, "dist", "wet-boew"),
         },
       ],
-    },
-  
-    optimization: {
-      minimize: true,
-      usedExports: true,
-      splitChunks: {
-        cacheGroups: {
-          highchartsVendor: {
-            test: /[\\/]node_modules[\\/](highcharts)[\\/]/,
-            chunks: "initial",
-            reuseExistingChunk: true,
-            enforce: true,
-            filename: "vendor/highcharts.[contenthash].js",
-          },
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      chunks: "all",
+      chunksSortMode: "auto",
+      template: "src/index.html",
+      minify: {
+        collapseWhitespace: false,
+        keepClosingSlash: false,
+        removeComments: false,
+        removeRedundantAttributes: false,
+        removeScriptTypeAttributes: false,
+        removeStyleLinkTypeAttributes: false,
+        useShortDoctype: false,
+      },
+    }),
+    // uncomment these lines below for easier browser debugging in development mode
+    new webpack.SourceMapDevToolPlugin({
+      filename: "dist/[file].map",
+      fileContext: "public",
+    }),
+    // new BundleAnalyzerPlugin(),
+  ],
+
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+    ],
+  },
+
+  optimization: {
+    minimize: true,
+    usedExports: true,
+    splitChunks: {
+      cacheGroups: {
+        highchartsVendor: {
+          test: /[\\/]node_modules[\\/](highcharts)[\\/]/,
+          chunks: "initial",
+          reuseExistingChunk: true,
+          enforce: true,
+          filename: "vendor/highcharts.[contenthash].js",
         },
       },
     },
-  };
+  },
+};
