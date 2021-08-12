eb1496520756d1f2350aa33b049392b9abbbc700
grant <grantmossdev@gmail.com>
2021-08-12 07:30:09 -0600

    updated data and dependencies

--- src/lang.js
+++ src/lang.js
@@ -16,7 +16,7 @@ export const lang = {
     refineries: "refineries",
     numberFormat: { seperator: " ", decimal: "." },
     updated: (now, next) =>
-      `Chart data updated on <strong>${now}</strong>. Next update: first week of <strong>${next}</strong>.`,
+      `Chart data updated on <strong>${now}</strong>. Next update: second week of <strong>${next}</strong>.`,
   },
   fr: {
     units: { imperial: "milliers de b/j", metric: "milliers de m3/j" },
@@ -35,6 +35,6 @@ export const lang = {
     refineries: "raffineries",
     numberFormat: { seperator: " ", decimal: "," },
     updated: (now, next) =>
-      `Données des graphiques mises à jour le <strong>${now}</strong>. Prochaine mise à jour : première semaine de <strong>${next}</strong>.`,
+      `Données des graphiques mises à jour le <strong>${now}</strong>. Prochaine mise à jour : seconde semaine de <strong>${next}</strong>.`,
   },
 };
